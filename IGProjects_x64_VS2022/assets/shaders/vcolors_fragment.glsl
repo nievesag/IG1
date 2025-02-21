c65990947fa74f5a676309c045ce5cc7f8bf8851
nievesag <nievalon@ucm.es>
2025-02-21 15:30:44 +0100

    -

--- IGProjects_x64_VS2022/assets/shaders/vcolors_fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-in vec4 vertexColor;  // color assigned to this vertex
-out vec4 FragColor;  // fixed-name color variable used by OpenGL
-
-void main()
-{
-	FragColor = vertexColor;
-}
